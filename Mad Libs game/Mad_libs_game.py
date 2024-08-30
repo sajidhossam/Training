79baf776264940c3fae73d8952e3ae61fcbdf97d
Sajid Hossam <sajidhossam1@gmail.com>
2024-08-30 22:04:28 +0300

    Mad LIb game

---
+++ Mad Libs game/Mad_libs_game.py
@@ -0,0 +1,24 @@
+place = input("Enter place: ")
+adjective1 = input("Enter adjective: ")
+adjective2 = input("Enter adjective: ")
+adjective3 = input("Enter adjective: ")
+noun1 = input("Enter noun: ")
+noun2 = input("Enter noun: ")
+noun3 =  input("Enter noun: ")
+noun4 = input("Enter noun: ")
+verb1 = input("Enter verb: ")
+verb2  = input("Enter verb: ")
+
+
+
+
+
+story = f"""
+Today I went to the {place}. I saw a {adjective1} {noun1} jumping up and down in a {noun2}. 
+It {verb1} through the large tunnel that led to its {adjective2} {noun3}. 
+Then, it {verb2} into the {adjective3} {noun4}.
+"""
+
+
+print("\nHere's your mad lib story")
+print(story)
